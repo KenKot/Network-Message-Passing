dcc5cf3511501597ee2b75121181e43cd4444c16
Kenny <kennykotten@gmail.com>
2025-04-15 20:19:09 -0700

    add try/catch to client

--- Client.java
+++ Client.java
@@ -1,36 +1,41 @@
+
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
 //	private static int count = 0;
 	private final String id;
 
-	private final int PORT;
-	private final String IP;
-	
-//	public Client(int PORT, String IP) { // w/o id
-	public Client(int PORT, String IP, String id) {
-		this.PORT = PORT;
-		this.IP = IP;
-		
-		this.id = id;
-		
+	private final int port;
+	private final String hostIP;
+
+	private final Socket socket;
+
+	public Client(int port, String hostIP, String id) throws IOException {
+		this.port = port;
+		this.hostIP = hostIP;
+		this.id = id; // will be from client argument
+
+		this.socket = new Socket(hostIP, port);
 
 		Message m1 = new Message(MessageStatusEnum.SUCCESS, MessageTypeEnum.LOGIN, "HELLO THERE");
 	}
-	
+
 //	TESTER - TO DELETE
 	public void log() {
 		System.out.println("LOGGER - TO DELETE");
-		System.out.println("Client ID: " + this.id + " PORT: " + this.PORT + " IP: " + this.IP);
-		
+		System.out.println("Client ID: " + this.id + " PORT: " + this.port + " IP: " + this.hostIP);
+
 	}
-	
+
 //	private void sendMessage() {}
-	
+
 	public static void main(String[] args) {
-		Client c1 = new Client(4321, "127.0.0.1", args[0]);
-		System.out.println("Client's main ran");
-		
-		c1.log();
-//		while(true) {}
-		
+		try {
+			Client c1 = new Client(4321, "127.0.0.1", "A");
+			c1.log();
+		} catch (IOException e) {
+			System.out.println("error connecting: " + e);
+		}
 	}
 }
