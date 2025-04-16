1261eb1b8351d0088cfc215f8bdd8b396c80888c
Kenny <kennykotten@gmail.com>
2025-04-15 19:59:09 -0700

    add id

--- Client.java
+++ Client.java
@@ -1,10 +1,16 @@
 public class Client {
+//	private static int count = 0;
+	private final String id;
+
 	private final int PORT;
 	private final String IP;
 	
-	public Client(int PORT, String IP) {
+//	public Client(int PORT, String IP) { // w/o id
+	public Client(int PORT, String IP, String id) {
 		this.PORT = PORT;
 		this.IP = IP;
+		
+		this.id = id;
 		
 
 		Message m1 = new Message(MessageStatusEnum.SUCCESS, MessageTypeEnum.LOGIN, "HELLO THERE");
@@ -13,17 +19,18 @@ public class Client {
 //	TESTER - TO DELETE
 	public void log() {
 		System.out.println("LOGGER - TO DELETE");
-		System.out.println("PORT: " + this.PORT + " IP: " + this.IP);
+		System.out.println("Client ID: " + this.id + " PORT: " + this.PORT + " IP: " + this.IP);
 		
 	}
 	
 //	private void sendMessage() {}
 	
 	public static void main(String[] args) {
-		Client c1 = new Client(4321, "127.0.0.1");
+		Client c1 = new Client(4321, "127.0.0.1", args[0]);
 		System.out.println("Client's main ran");
 		
 		c1.log();
+//		while(true) {}
 		
 	}
 }
